96a9fef9b77c0e293e54e80e679f4bbb1e85fe63
Anthony Nixon <ajn0592@gmail.com>
2017-05-03 23:19:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.debateflowsapi = function debateflowsapi(req, res) {
+
+  console.log(req);
+  res.status(200).send('pong');
+};

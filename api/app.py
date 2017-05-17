33372a08e91cf439a9e1a4f698bd0cf3fbd38750
Anthony Nixon <ajn0592@gmail.com>
2017-05-16 23:03:40 -0500

    allow for trailing slashes

--- api/app.py
+++ api/app.py
@@ -9,19 +9,19 @@ def index():
     return "Hello, World!"
 
 
-@app.route('/v1/users', methods=['GET'])
+@app.route('/v1/users', methods=['GET'], strict_slashes=False)
 def get_users():
     (status, data) = users.get_users()
     return jsonify(data), status
 
 
-@app.route('/v1/users/<int:user_id>', methods=['GET'])
+@app.route('/v1/users/<int:user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     (status, data) = users.get_user(user_id)
     return jsonify(data), status
 
 
-@app.route('/v1/users', methods=['POST'])
+@app.route('/v1/users', methods=['POST'], strict_slashes=False)
 def new_user():
     (status, data) = users.new_user(request.json)
     return jsonify(data), status

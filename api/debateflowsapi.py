e28ab99a9f0190621679f8a324f7ecfaa19bb9d8
Anthony Nixon <ajn0592@gmail.com>
2017-05-17 21:14:35 -0500

    i had a typo

--- api/debateflowsapi.py
+++ api/debateflowsapi.py
@@ -26,7 +26,7 @@ def new_user():
     (status, data) = users.new_user(request.json)
     return jsonify(data), status
 
-@app.route('/v1/login', methods=['POST'], strict_slashed=False)
+@app.route('/v1/login', methods=['POST'], strict_slashes=False)
 def auth_user():
     (status, data) = login.authenticate(request.json)
     return jsonify(data), status

ef8dd882ecd30b64ed5611cd81b9cc0e7705eb89
Anthony Nixon <ajn0592@gmail.com>
2017-05-17 22:34:28 -0500

    im trying anything at this point

--- api/debateflowsapi.py
+++ api/debateflowsapi.py
@@ -1,11 +1,10 @@
 #!flask/bin/python
 from drivers import users, login
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')

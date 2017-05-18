0ebb252fbf1bfd4a2f1177e3bfb95718a2c14f3f
Anthony Nixon <ajn0592@gmail.com>
2017-05-17 22:38:08 -0500

    i think cors broke everything

--- api/debateflowsapi.py
+++ api/debateflowsapi.py
@@ -1,10 +1,8 @@
 #!flask/bin/python
 from drivers import users, login
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route('/')
